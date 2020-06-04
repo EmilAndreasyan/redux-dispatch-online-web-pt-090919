af078b36ebdd917de22492f779ce1d30a4ae8692
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-04 06:33:07 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,4 @@
+let state = {count: 0}
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,20 +8,21 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
 changeState(state, action)
 
-function dispatch(action){
-  state = changeState(state, action)
-  return state
-}
-
-dispatch({type: 'INCREASE_COUNT'})
-dispatch({type: 'INCREASE_COUNT'})
-
 function render(){
   document.body.textContent = state.count
 }
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+dispatch({type: 'INCREASE_COUNT'})
+dispatch({type: 'INCREASE_COUNT'})
+
+
 render()
